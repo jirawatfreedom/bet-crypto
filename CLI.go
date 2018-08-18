6645624992a16c6c12dda6e3db60370606afce60
Jirawat <jirawat@entercorp.net>
2018-08-18 14:07:18 +0700

    add schedule

--- CLI.go
+++ CLI.go
@@ -4,12 +4,26 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
 
 // CLI helps players through a game of poker
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+}
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
 }
 
 // NewCLI creates a CLI for playing poker
--- CLI_test.go
+++ CLI_test.go
@@ -41,6 +41,16 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(playerStore, in)
 		cli.PlayPoker()
 	})
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+		if len(blindAlerter.alerts) != 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
 
 }
 
